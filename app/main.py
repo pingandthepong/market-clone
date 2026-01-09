09865b3614255f80f5645ae7ebd79769dc1d1adf
pingandthepong <pingandthepong@gmail.com>
2026-01-09 16:15:51 +0900

    [feat] 글쓰기 form 데이터 POST & DB table 생성

--- app/main.py
+++ app/main.py
@@ -2,6 +2,10 @@ from fastapi import FastAPI, UploadFile, Form
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import Annotated
+import sqlite3
+
+con = sqlite3.connect("db.db", check_same_thread=False)
+cur = con.cursor()
 
 app = FastAPI()
 
@@ -22,14 +26,19 @@ def read_chat():
 
 # write (내 물건 팔기)
 @app.post("/items")
-def create_item(
-  image: UploadFile,
-  title: Annotated[str, Form()], # Form 데이터 형식으로, 문자열로 온다.
-  description: Annotated[str, Form()],
-  price: Annotated[int, Form()],
-  place: Annotated[str, Form()]
-  ):
-  print(image, title, description, price, place)
+async def create_item(image: UploadFile,
+                title: Annotated[str, Form()],
+                description: Annotated[str, Form()],
+                price: Annotated[int, Form()],
+                place: Annotated[str, Form()]
+               ):
+
+  image_bytes = await image.read()
+  cur.execute(f"""
+              INSERT INTO items(image, title, description, price, place)
+              VALUES ('{image_bytes.hex()}', '{title}', '{description}',{price}, '{place}')
+              """)
+  con.commit()
   return "200"
 
 app.mount("/", StaticFiles(directory="frontend", html=True), name="frontend")
--- frontend/script/write.js
+++ frontend/script/write.js
@@ -1,15 +1,18 @@
-// form에서 데이터가 전송됐을 때, 그 데이터를 서버에 보내기
 const form = document.querySelector("#write-form");
 
 const handleSubmitForm = async (e) => {
   e.preventDefault();
 
-  await fetch("/items", {
-    method: "POST",
-    body: new FormData(form),
-  });
-
-  console.log("제출완료");
+  try {
+    const res = await fetch("/items", {
+      method: "POST",
+      body: new FormData(form),
+    });
+    const data = await res.json();
+    if (data === "200") window.location.pathname = "/";
+  } catch (e) {
+    console.error(e);
+  }
 };
 
 form.addEventListener("submit", handleSubmitForm);
