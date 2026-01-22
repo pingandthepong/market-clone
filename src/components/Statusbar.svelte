8a39442c5f463417e25f6b619833f57c8e310ae8
pingandthepong <pingandthepong@gmail.com>
2026-01-22 22:56:01 +0900

    refactor: Statusbar svelte로 마이그레이션

--- src/components/Statusbar.svelte
+++ src/components/Statusbar.svelte
@@ -1,3 +1,26 @@
+<script>
+  const hour = new Date().getHours();
+  const minutes = new Date().getMinutes();
+  const time = `${hour}:${minutes}`;
+
+  // Battery Level (Chrome, Edge, Opera 지원)
+  let userBatteryLevel;
+
+  if ("getBattery" in navigator) {
+    navigator.getBattery().then((battery) => {
+      const updateBattery = () => {
+        userBatteryLevel = `${Math.round(battery.level * 100)}%`;
+      };
+
+      updateBattery(); // 초기값
+      battery.addEventListener("levelchange", updateBattery);
+    });
+  } else {
+    console.log("배터리 정보 미지원 브라우저");
+    alert("배터리 정보 미지원 브라우저입니다. 임의의 값으로 표현됩니다.");
+  }
+</script>
+
 <div class="status-bar">
   <div class="status-bar__wrap">
     <img src="/assets/signal.svg" alt="" />
@@ -5,12 +28,14 @@
     <img src="/assets/wifi.svg" alt="" />
   </div>
   <div class="status-bar__wrap">
-    <span class="status-bar__text fw-semi-bold" id="currentTime">HH:MM</span>
+    <span class="status-bar__text fw-semi-bold" id="currentTime">{time}</span>
   </div>
   <div class="status-bar__wrap">
     <img class="security" src="/assets/security.svg" alt="" />
     <img class="alarm" src="/assets/alarm.svg" alt="" />
-    <span class="status-bar__text" id="userBattery">50%</span>
+    <span class="status-bar__text" id="userBattery">
+      {userBatteryLevel ?? "50%"}
+    </span>
     <img class="battery" src="/assets/battery.svg" alt="" />
   </div>
 </div>
